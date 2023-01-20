9b1580802b1feed96199d1155c8e5bac3d410adb
장희용 <ralf1204@com2us.com>
2023-01-20 16:40:16 +0900

    [Release] 0.1.2

--- Runtime/HelloWorld.cs
+++ Runtime/HelloWorld.cs
@@ -7,6 +7,7 @@ public class s : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("test");
+        Debug d = new Debug();
+        d.Log("test");
     }
 }
